ebc34ecdfa5f8038266d21ae90e6a8edb4aed22e
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-05-20 22:25:09 +0530

    Added. New Contact in AddressBook System.

--- AddressBook.js
+++ AddressBook.js
@@ -1,6 +1,9 @@
 /**Welcome To Address Book Program 
  * Creating a contact with first name , last name 
  * address, city , state , zip , phone number and email
+ * Checking contacts to be in proper pattern 
+ * Throw error if regex fails
+ * Created new addressbook array and push the contacts in it
 */
 /** Created Contact Class */
 class Contact {
@@ -13,7 +16,7 @@ class Contact {
     zip;
     mobileNumber;
     email;
-/** Created Constructor */
+    /** Created Constructor */
     constructor (firstName,lastName,address,city,state,zip,mobileNumber,email) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -23,8 +26,106 @@ class Contact {
         this.zip = zip;
         this.mobileNumber = mobileNumber;
         this.email = email;
+    }    
+
+    /* @Description - to validate first name should start with captital character
+     * to used throw keyword for the invalid first name */
+
+    get firstName() { return this.firstName; }
+    set firstName(firstName) {
+        let nameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+        if (nameRegex.test(firstName))
+            this.firstName = firstName;
+        else
+            throw "Invalid first Name";
     }
-/** ToString Method for Printing Result */
+
+    /* @Description - to validate last name should start with captital character
+    * to used throw keyword for the invalid last name */
+
+    get lastName() { return this.lastName; }
+    set lastName(lastName) {
+        let nameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+        if (nameRegex.test(lastName))
+        this.lastName = lastName;
+        else
+            throw "Invalid  last Name";
+    }
+
+    /* @Description - to validate address should start with captital letters or small letters
+    * to used throw keyword for the invalid address */
+
+    get address() { return this.address; }
+    set address(address) {
+        let addressRegex = RegExp("^[A-Za-z]{4,}$");
+        if (addressRegex.test(address))
+            this.address = address;
+        else
+            throw "Invalid address ";
+    }
+
+    /* @Description - to validate city should start with captital letters or small letters
+    * to used throw keyword for the invalid city  */
+
+    get city() { return this.city; }
+    set city(city) {
+        let cityRegex = RegExp("^[A-Za-z]{4,}$");
+        if (cityRegex.test(city))
+            this.city = city;
+        else
+            throw "Invalid city ";
+    }
+
+    /* @Description - to validate state should start with captital letters or small letters
+    * to used throw keyword for the invalid state  */
+
+    get state() { return this.state; }
+    set state(state) {
+        let stateRegex = RegExp("^[A-Za-z0-9]{4,}$");
+        if (stateRegex.test(state))
+            this.state = state;
+        else
+            throw "Invalid state";
+    }
+
+    /* @Description - to validate zip  should start with numbers 
+    * to used throw keyword for the invalid state  */
+
+    get zip() { return this.zip; }
+    set zip(zip) {
+        let zipRegex = RegExp("^[1-9]{3}[ ]?[0-9]{3}$");
+        if (zipRegex.test(zip))
+        this.zip = zip;
+        else
+            throw "Invalid zip ";
+    }
+
+    /* @Description - to validate phone number should start with numbers to allow max 10 digit
+    * to used throw keyword for the invalid phone number  */
+
+    get mobileNumber() { return this.mobileNumber; }
+    set mobileNumber(mobileNumber) {
+        let phoneRegex = RegExp("^[1-9]{2}[ ]{1}[0-9]{10}$");
+        if (phoneRegex.test(mobileNumber))
+            this.mobileNumber = mobileNumber;
+        else
+            throw "Invalid phone number";
+    }
+
+    /* @Description - to validate email id  should start with small letter to allow one number and one
+    * special character. 
+    * to used throw keyword for the invalid phone number  */
+
+    get email() { return this.email; }
+    set email(email) {
+        let emailRegex = RegExp("^([a-z0-9A-Z])+([.]?[a-z0-9A-Z]+)*[@]{1}[a-z0-9A-Z]+[.]{1}[a-zA-Z]{2,}([.]{1}[a-z]{2,})?$");
+        if (emailRegex.test(email))
+            this.email = email;
+        else
+            throw "Invalid email";
+    }  
+
+    /** ToString Method for Printing Result */
     toString() {
         return "\nFirst Name    : " + this.firstName +
         "\nLast Name     : " + this.lastName +
@@ -35,9 +136,20 @@ class Contact {
         "\nMobile Number : " + this.mobileNumber +
         "\nEmail         : " + this.email;
     }
-}
-console.log("<<<<<Welcome to AddressBook Application>>>>>");
+}    
+    /* @Description - to create a new address book array and add new contacts  */
+    try {        
+        var addressBook = new Array();
+        console.log("<<<<<Welcome to AddressBook Application>>>>>");
+        addressBook.push(new Contact("Ashish","Rathod","Gangakhed","Parbhani","Maharashtra",431514,8805925703,"Ashu03.mit@gmail.com")); 
+        addressBook.push(new Contact("Sakha","Rathod","Gangakhed","Parbhani","Maharashtra",431514,8788594431,"Sakha2111@gmail.com"));         
+        addressBook.forEach((contact) => console.log(contact.toString()));
+        }
+        catch (e) {
+            console.log('Regex test is fail \n' + e);
+        }
 
-/** UC1 - Ability to Create Contact in Address Book */
-let contact = new Contact("Ashish","Rathod","Gangakhed","Parbhani","Maharashtra",431514,8805925703,"Ashu03.mit@gmail.com");
-console.log(contact.toString());
+ 
+
+      
+    
